0b260caebb910f72314bbb4c852abb887864ab86
David Wu <davidcnwu@gmail.com>
2014-01-25 15:37:57 -0800

    Adding successful test case!

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
+  render_views
+
   describe "#index" do
     it "returns the correct response" do
-      binding.pry
-      user = FactoryGirl.create(:user)
-      binding.pry
+      2.times { FactoryGirl.create(:user) }
 
       get :index
 
       expect(response).to be_successful
+
+      json = JSON.parse(response.body)
+      user_ids = json["users"].map {|user_hash| user_hash["id"]}
+
+      expect(user_ids).to match_array(User.pluck(:id))
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,9 @@ FactoryGirl.define do
   factory :user do
     first_name "David"
     last_name "Wu"
-    email "test@gmail.com"
-    # sequence :email do |n|
-    #   "test#{n}@gmail.com"
-    # end
+    sequence :email do |n|
+      "test#{n}@gmail.com"
+    end
     password "testtest1234"
   end
 end
