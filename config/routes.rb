9e91cd6f90c2f8ec8eaa22f83bf59e45621f5f0b
David Wu <davidcnwu@gmail.com>
2014-03-11 02:09:01 -0700

    Ahh, committing a bunch of stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Peanut::Application.routes.draw do
   devise_for :users
-  root :to => "home#index"
+  root :to => "welcome#index"
+
+  # get 'users/:username', to: 'users#show'
 
   namespace :api do
     namespace :v1 do
